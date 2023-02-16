a4d113dd063210d86ba1e012c621866e6974cfa9
limecats <limecats0331@gmail.com>
2023-02-16 17:51:32 +0900

    Initial commit

---
+++ src/test/java/edu/limecats/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.limecats.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
